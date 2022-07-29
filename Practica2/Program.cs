1223919224eb04079216844f68ea7b6cf5539503
cindy <MINEDUCYT@CINDY-VALENCIA>
2022-07-29 09:57:36 -0600

    Segundo programa

--- Practica2/Program.cs
+++ Practica2/Program.cs
@@ -6,7 +6,17 @@ namespace Practica2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declaracion de variables
+            int num1 = 5;
+            int num2 = 4;
+            var total = num1 + num2;
+
+            string name = "Enrique";
+            int edad = 27;
+
+            //Impresion de datos
+            Console.WriteLine("La suma es:" + total);
+            Console.WriteLine(name + " " + edad);
         }
     }
 }
---
+++ Practica3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practica3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables
+
+            string firstname = "Cindy";
+            String lastname = "Valencia";
+            int edad = 37;
+            //Concatenacion de variables
+            var name = firstname + " " + lastname;
+            //Imprimir variables dentro del texto
+            Console.WriteLine("Su nombre es: {0} y su edad es: {1}", name, edad);
+        }
+    }
+}
